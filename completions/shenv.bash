ea1aad90f0d2f04ca0a80c10be1afc8e705a758b
Ville Skyttä <ville.skytta@iki.fi>
2020-05-26 22:28:47 +0300

    bash completion: avoid unintentional globbing

--- completions/shenv.bash
+++ completions/shenv.bash
@@ -6,8 +6,8 @@ _shenv() {
     COMPREPLY=( $(compgen -W "$(shenv commands)" -- "$word") )
   else
     local words=("${COMP_WORDS[@]}")
-    unset words[0]
-    unset words[COMP_CWORD]
+    unset "words[0]"
+    unset "words[COMP_CWORD]"
     local completions=$(shenv completions "${words[@]}")
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
